3b687fef8033b30a30077228b0d8eaf21919abcb
Saravanane MANICOME <saravanane.manicome@carbon-it.com>
2022-10-25 16:58:35 +0200

    fix: change test name

--- src/test/java/io/smanicome/bank/service/OperationsServiceShould.java
+++ src/test/java/io/smanicome/bank/service/OperationsServiceShould.java
@@ -30,7 +30,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class OperationsServiceTest {
+class OperationsServiceShould {
     @Mock
     private OperationsDao operationsDao;
 
