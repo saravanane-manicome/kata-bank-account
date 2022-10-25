97e010070b7839debfe7fadcae213952cdd50f76
Saravanane MANICOME <saravanane.manicome@carbon-it.com>
2022-10-25 17:09:15 +0200

    fix: update BankService to match requirements

--- src/main/java/io/smanicome/bank/service/BankService.java
+++ src/main/java/io/smanicome/bank/service/BankService.java
@@ -2,9 +2,12 @@ package io.smanicome.bank.service;
 
 import io.smanicome.bank.account.Amount;
 import io.smanicome.bank.account.Operation;
+import io.smanicome.bank.exceptions.NotEnoughBalanceException;
 
 import java.util.UUID;
 
 public interface BankService {
     Operation deposit(UUID accountId, Amount amount);
+    Operation withdraw(UUID accountId, Amount amount) throws NotEnoughBalanceException;
+    void printAccountStatement(UUID accountId);
 }
